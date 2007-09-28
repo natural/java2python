96cde0deaa208e385e5d672475a1209cbf84ce2b
Ling Li <lilingv@gmail.com>
2007-09-28 08:47:11 +0000

    Make switches testcases use python unittest and add some new ones.

--- java2python/tests/Switches.java
+++ java2python/tests/Switches.java
@@ -1,29 +1,66 @@
-class Switches {
-    public static void main(String[] args) {
-	System.out.println( switches() );
+import junit.framework.*;
+
+public class Switches extends TestCase {
+
+    public void testSwitches() {
+        Assert.assertEquals(switch1(1), 1);
+        Assert.assertEquals(switch1(2), 2);
+        Assert.assertEquals(switch1(3), 10);
+        Assert.assertEquals(switch1(10), 4);
+
+        Assert.assertEquals(switch2(3), 2);
+        Assert.assertEquals(switch2(3), 2);
+
+        Assert.assertEquals(switch3(3), 4);
+
+        Assert.assertEquals(switch4(1), 1);
+        Assert.assertEquals(switch4(3), 2);
     }
 
-    public static int switches() {
-        int i;
-	i = 3;
-
+    public int switch1(int i) {
         switch (i) {
             case 1:
             case 2:
                 break;
             case 3:
-                i = 2;
+                i = 10;
+                break;
             default:
-                return -4;
+                i = 4;
         }
+        return i;
+    }
 
+    public int switch2(int i) {
         switch (i) {
             case 3:
             default:
                 i = 2;
-	}
-        return -1;
+        }
+        return i;
+    }
+
+    public int switch3(int i) {
+        switch (i) {
+            case 1:
+            case 2:
+                break;
+            case 3:
+                i = 10;
+            default:
+                i = 4;
+        }
+        return i;
+    }
+
+    public int switch4(int i) {
+        switch (i) {
+            case 1:
+                if (i == 1) break;
+            default:
+                i = 2;
+        }
+        return i;
     }
 
 }
-
