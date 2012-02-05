d470aba0dd74208e53c0a4ce6f6819696b9e8ae1
Troy Melhase <troy@gci.net>
2012-02-05 15:09:40 -0500

    First pass at setup.py and install.md.

---
+++ setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" setup.py -> java2python setup script.
+
+Simple but effective tool to translate Java source code into Python.
+
+This package provides tools to imperfectly translate Java source code to
+Python source code.
+
+This version requires Python 2.7.
+"""
+
+from distutils.core import setup
+
+
+classifiers = """
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+Natural Language :: English
+Operating System :: OS Independent
+Operating System :: POSIX
+Programming Language :: Python
+Programming Language :: Java
+Topic :: Software Development
+Topic :: Software Development :: Code Generators
+"""
+
+
+description = __doc__.split('\n')[2]
+long_description = '\n'.join(__doc__.split('\n')[4:])
+
+
+setup(
+    name='java2python',
+    version='0.5',
+
+    description=description,
+    long_description=long_description,
+
+    author='Troy Melhase',
+    author_email='troy@troy.io',
+
+    url='https://github.com/natural/java2python/',
+    download_url='https://github.com/downloads/natural/java2python/java2python-0.5.tar.gz',
+
+    keywords=['java', 'java2python', 'compiler'],
+    classifiers=filter(None, classifiers.split('\n')),
+
+    packages=[
+        'java2python',
+        'java2python.compiler',
+        'java2python.config',
+        'java2python.lang',
+        'java2python.lib',
+        'java2python.mod',
+        ],
+
+    package_data={
+        'java2python.lang': [
+            '*.g',
+            '*.tokens',
+            ]
+        },
+
+    scripts=[
+        'bin/j2py',
+        ],
+
+    install_requires=['antlr_python_runtime=3.1.3'],
+
+    )
