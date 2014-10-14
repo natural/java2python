39d019aa2d3468aa45d3ce050ad81caa8d01b3a5
Brian Kearns <bdkearns@gmail.com>
2014-10-13 22:56:12 -0400

    use functools.wraps for synchronized decorator

--- java2python/mod/include/sync.py
+++ java2python/mod/include/sync.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from threading import RLock
 
 def lock_for_object(obj, locks={}):
@@ -5,6 +6,7 @@ def lock_for_object(obj, locks={}):
 
 def synchronized(call):
     assert call.__code__.co_varnames[0] in {'self', 'cls'}
+    @wraps(call)
     def inner(*args, **kwds):
         with lock_for_object(args[0]):
             return call(*args, **kwds)
