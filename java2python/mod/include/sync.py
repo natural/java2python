af2b2e44ded258841035c32ed35f5269663679f9
Brian Kearns <bdkearns@gmail.com>
2014-10-13 22:34:19 -0400

    fix synchronized methods to lock per-instance not per-method

--- java2python/mod/include/sync.py
+++ java2python/mod/include/sync.py
@@ -1,12 +1,11 @@
 from threading import RLock
 
-_locks = {}
-def lock_for_object(obj, locks=_locks):
+def lock_for_object(obj, locks={}):
     return locks.setdefault(id(obj), RLock())
 
-
 def synchronized(call):
+    assert call.__code__.co_varnames[0] in {'self', 'cls'}
     def inner(*args, **kwds):
-        with lock_for_object(call):
+        with lock_for_object(args[0]):
             return call(*args, **kwds)
     return inner
